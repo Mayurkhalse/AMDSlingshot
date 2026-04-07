0496e7ebba41c08518122a1442daa1ee1cd5a5b2
Mayurkhalse <163679044+Mayurkhalse@users.noreply.github.com>
2026-04-07 16:31:50 +0530

    fix express v5 routing issue

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "dist")));
 
 // React routing support
-app.get("*", (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
